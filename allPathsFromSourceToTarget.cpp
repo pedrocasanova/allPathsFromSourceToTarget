ac62d9fd26821ee852ac58f6252ac1a7ff82bd58
pedrocasanova <jpsoc.1996@gmail.com>
2022-10-27 23:59:58 +0100

    Added initial push of elements

--- allPathsFromSourceToTarget.cpp
+++ allPathsFromSourceToTarget.cpp
@@ -4,16 +4,19 @@ public:
     {
         vector<vector<int>> paths;
         vector<int> connected;
-        int i = 0;
+        int i = 0, lastNode = 3;
         connected = graph[0];
-        while(!connected.empty())
+        // visit each node connected to 0
+        for (int i = 0; i < connected.size(); i++)
         {
-            for(int j = 0; j < graph[connected[i]].size(); j++)
+            paths.push_back({0});
+            for(int j = 0; j < graph[i].size(); j++)
             {
-                connected.emplace_back(graph[connected[i]][j]);
-            }
-            connected.erase(connected.begin());
-            i++;
+                // paths[i].push_back(graph[j][i]);
+                cout << graph[i][j] << " , ";
+                
+            } 
+            cout << endl;
         }
         return paths;
     }
