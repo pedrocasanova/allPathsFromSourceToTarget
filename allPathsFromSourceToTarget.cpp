0ff92166da485b24e54732b509fe2e5b238b3166
pedrocasanova <jpsoc.1996@gmail.com>
2022-11-02 19:46:41 +0000

    Added loop to arrive at second layer of nodes
    Added loop to arrive at second layer of nodes

--- allPathsFromSourceToTarget.cpp
+++ allPathsFromSourceToTarget.cpp
@@ -3,20 +3,46 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
     {
         vector<vector<int>> paths;
-        vector<int> connected;
-        int i = 0, lastNode = 3;
-        connected = graph[0];
+        vector<int> initialNodesToVisit, nextNodesToVisit, proposedPath;
+        int j = 0, lastNode = graph.size() - 1;
+        initialNodesToVisit = graph[0];
         // visit each node connected to 0
-        for (int i = 0; i < connected.size(); i++)
+        for (int i = 0; i < initialNodesToVisit.size(); i++)
         {
-            paths.push_back({0});
-            for(int j = 0; j < graph[i].size(); j++)
+            proposedPath.emplace(proposedPath.begin(), 0);
+            if(initialNodesToVisit[i] == lastNode)
             {
-                // paths[i].push_back(graph[j][i]);
-                cout << graph[i][j] << " , ";
-                
-            } 
-            cout << endl;
+                proposedPath.emplace_back(initialNodesToVisit[i]);
+                paths.emplace_back(proposedPath);
+                proposedPath.clear();
+            }
+            else
+            {
+                nextNodesToVisit = graph[i];
+                for(int k = 0; k < nextNodesToVisit.size(); k++) 
+                {
+                        cout << "next nodes to visit:  " << nextNodesToVisit[k] << endl;
+                }
+                for (int j = 0; j < nextNodesToVisit.size(); j++)
+                {
+                    if (nextNodesToVisit[j] == lastNode)
+                    {
+                        proposedPath.emplace_back(nextNodesToVisit[j]);
+                        paths.emplace_back(proposedPath);
+                        proposedPath.pop_back();
+                    }
+                    else if(nextNodesToVisit.empty())
+                    {
+                        proposedPath.clear();
+                        break;
+                    }
+                    else
+                    {
+                        proposedPath.emplace_back(nextNodesToVisit[j]);
+                        nextNodesToVisit = graph[j];
+                    }
+                }
+            }
         }
         return paths;
     }
